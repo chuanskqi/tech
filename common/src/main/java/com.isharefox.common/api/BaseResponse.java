b7394b7c7cf0c979f42a0f26e658e6855482265c
chuanskqi <shenkang777@163.com>
2020-11-16 23:47:14 +0800

    Initial commit

---
+++ common/src/main/java/com.isharefox.common/api/BaseResponse.java
@@ -0,0 +1,21 @@
+package com.isharefox.common.api;
+
+import com.isharefox.common.api.ResultCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BaseResponse {
+    private String message;
+    @Builder.Default
+    private ResultCode code = ResultCode.SUCCESS;
+
+    public boolean isSuccess() {
+        return code == ResultCode.SUCCESS;
+    }
+}
